250a023023990fb56c95d89076d1ef31dbf4b665
Leonardo <leorochaa2@gmail.com>
2014-03-23 17:42:40 -0300

    Commit Leo 300

--- Projeto_FSJ/src/HelloFSJ.java
+++ Projeto_FSJ/src/HelloFSJ.java
@@ -3,11 +3,11 @@ public class HelloFSJ {
 
 	public static void main(String[] args) {
 		System.out.println("Ol� FSJ");
+		System.out.println("Modificação Filipe");
 		System.out.println("Ol� FSJ");
 		System.out.println("Ol� FSJ");
-		System.out.println("Ol� FSJ");
-		System.out.println("Ol� FSJ");
-		System.out.println("Ol� FSJ by Leonardo");
+		System.out.println("4");
+		System.out.println("Modificação Leonardo");
 	}
 
 }
