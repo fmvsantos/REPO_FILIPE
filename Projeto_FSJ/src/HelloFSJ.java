5f3b80d030f861201fd18d2abdd6897dc9612281
fmvsantos <filipemvsantos@yahoo.com.br>
2014-03-23 17:55:08 -0300

    Merge feito e comitado

--- Projeto_FSJ/src/HelloFSJ.java
+++ Projeto_FSJ/src/HelloFSJ.java
@@ -4,9 +4,9 @@ public class HelloFSJ {
 	public static void main(String[] args) {
 		System.out.println("Ol� FSJ");
 		System.out.println("Modificação Filipe");
+		System.out.println("2");
 		System.out.println("Ol� FSJ");
-		System.out.println("Ol� FSJ");
-		System.out.println("Ol� FSJ");
+		System.out.println("Ol� FSJ 5k");
 		System.out.println("Ol� FSJ by Leonardo");
 	}
 
