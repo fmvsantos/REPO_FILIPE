a31fcd00c75b7d278befd85969fb02fb5aeee20e
Leonardo <leorochaa2@gmail.com>
2014-03-24 11:07:40 -0300

    New Class - Léo

---
+++ Projeto_FSJ/src/CopyOfHelloFSJ.java
@@ -0,0 +1,13 @@
+
+public class CopyOfHelloFSJ {
+
+	public static void main(String[] args) {
+		System.out.println("Ol� FSJ");
+		System.out.println("Modificação Filipe");
+		System.out.println("Ol� FSJ");
+		System.out.println("Ol� FSJ");
+		System.out.println("Ol� FSJ 5k");
+		System.out.println("Ol� FSJ by Leonardo");
+	}
+
+}
