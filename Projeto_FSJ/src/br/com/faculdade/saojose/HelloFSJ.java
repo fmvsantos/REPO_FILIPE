e06db94734f5f9557ffe588d303ef93446a167a2
Leonardo <leorochaa2@gmail.com>
2014-03-24 14:42:43 -0300

    Refactor!

---
+++ Projeto_FSJ/src/br/com/faculdade/saojose/HelloFSJ.java
@@ -0,0 +1,12 @@
+package br.com.faculdade.saojose;
+
+public class HelloFSJ {
+	
+	public static void main(String[] args) {
+		hello();
+	}
+	
+	private static void hello(){
+		System.out.println("Hello São José!");
+	}
+}
