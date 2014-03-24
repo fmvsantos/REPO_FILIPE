026569b0158cfa3589dd2c7e213cd44a486800b8
Leonardo <leorochaa2@gmail.com>
2014-03-24 15:30:09 -0300

    Commit - Exercicio 1

--- Projeto_FSJ/src/br/com/faculdade/saojose/HelloFSJ.java
+++ Projeto_FSJ/src/br/com/faculdade/saojose/HelloFSJ.java
@@ -1,12 +1,42 @@
 package br.com.faculdade.saojose;
 
+import java.util.Scanner;
+
 public class HelloFSJ {
-	
+
+	private static String OPCAO_CONTINUAR_PROGRAMA = "";
+
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		hello();
+
+		String aluno = "";
+
+		do {
+			
+			hello(aluno);
+
+			System.out.println("Deseja continuar (s/n) ? \n");
+
+			Scanner operacao = new Scanner(System.in);
+			OPCAO_CONTINUAR_PROGRAMA = operacao.next();
+
+		} while ("s".equalsIgnoreCase(OPCAO_CONTINUAR_PROGRAMA));
+
+		fimPrograma();
 	}
-	
-	private static void hello(){
-		System.out.println("Hello São José!");
+
+	@SuppressWarnings("resource")
+	private static void hello(String aluno) {
+
+		Scanner nomeUsuario = new Scanner(System.in);
+		
+		System.out.print("Digite seu nome:");
+		aluno = nomeUsuario.next();
+
+		System.out.println("\n" + aluno + ", Hello São José! \n");
+	}
+
+	public static void fimPrograma() {
+		System.out.println("******** FIM DO PROGRAMA ********");
 	}
 }
